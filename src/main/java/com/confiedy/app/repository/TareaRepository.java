53eafa499cfdfe42462f4454519d8a796b3b6d77
Estrada <cesar.estrada@sap.com>
2024-03-19 02:00:02 -0500

    add tasks

--- src/main/java/com/confiedy/app/repository/TareaRepository.java
+++ src/main/java/com/confiedy/app/repository/TareaRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface TareaRepository extends JpaRepository<Docente, Integer> {
 
-    @Query(value = "SELECT d from DocenteTarea dt join "+
+    @Query(value = "SELECT t from DocenteTarea dt join "+
             "Tarea t on dt.tarea.id = t.id WHERE dt.docente.id = :docenteId")
     List<Tarea> findTareasByDocente(Integer docenteId);
 }
